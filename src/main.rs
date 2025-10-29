a79fd318a5b91b6cc8b3d7374b207267194426e3
tdh0ang <td.hoang03@gmail.com>
2025-10-29 20:53:00 +0100

    feat: added pulldown_cmark basic parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 use axum::{routing::get, Router};
-
+use std::{fs::File, io::Read};
+use pulldown_cmark::{Parser, Options};
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+async fn main() -> std::io::Result<()>{
+
+    /* pulldown_cmark base code */
+    let mut markdown = File::open("notes/info.md")?;
+    let mut contents = String::new();
+    let _ = markdown.read_to_string(&mut contents);
+
+    let mut options = Options::empty();
+    let parser = Parser::new_ext(&contents, options);
+
+    let mut html_output = String::new();
+    pulldown_cmark::html::push_html(&mut html_output, parser);
+
+
+    /* axum base code */
+    let app = Router::new()
+        .route("/", get(|| async { html_output }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
